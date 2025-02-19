f52651a1690859ea9f0ed298c5bcdcf97db76911
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2025-02-19 15:19:17 -0700

    updates

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,21 +1,37 @@
 import projects from '../data/data.json';
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 
 const WorkPage = () => {
   return (
-    <div className="container flex flex-col justify-center text-center">
-            <h1 className="m-2 text-2xl font-bold text-center">Projects</h1>
-            <ul>
-                {projects.map((project)=>(
-                    <li key={project.id} className="border-2 p-2 m-2">
-                        <Link href={`/work/${project.id}`}>{project.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    
+    <>
+    <h1 className="m-2 text-2xl font-bold text-center">Projects</h1>
+    <div className="container flex text-center">
+      
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id} className="border-2 bg-[#fff] p-2 m-2">
+            {/* Polaroid-style image */}
+            <div className="relative w-48 h-auto mx-auto mb-2" >
+              <Image
+                src={project.image}  // Corrected to use project.image
+                width={200}
+                height={200} // Same width and height to keep it square-like
+                alt="Project image"
+                className="object-cover"
+              />
+              {/* Adding a 'caption' effect to the bottom of the image */}
+              <div className="absolute bottom-0 w-full text-center bg-white px-2 text-xs text-gray-700">{project.title}</div>
+            </div>
+            <Link href={`/work/${project.id}`}>{project.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    </>
 );
 };
 
